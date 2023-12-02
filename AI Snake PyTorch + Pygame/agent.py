faff53c873f4770d5093e47e2154eb3f8e9e23e1
XxJaviMadxX <javierortizmi@gmail.com>
2023-12-02 17:30:43 -0500

    Minor changes and model saved

--- AI Snake PyTorch + Pygame/agent.py
+++ AI Snake PyTorch + Pygame/agent.py
@@ -21,7 +21,7 @@ class Agent:
         self.model = Linear_QNet(11, 256, 3)
         self.trainer = QTrainer(self.model, lr=LR, gamma=self.gamma)
     
-    def get_state(self, game: SnakeGameAI):
+    def get_state(self, game):
         head = game.snake[0]
         
         point_l = Point(head.x - 20, head.y)
@@ -36,34 +36,34 @@ class Agent:
 
         state = [
             # Danger straight
-            (dir_r and game.is_collision(point_r)) or
-            (dir_l and game.is_collision(point_l)) or
-            (dir_u and game.is_collision(point_u)) or
+            (dir_r and game.is_collision(point_r)) or 
+            (dir_l and game.is_collision(point_l)) or 
+            (dir_u and game.is_collision(point_u)) or 
             (dir_d and game.is_collision(point_d)),
-            
+
             # Danger right
-            (dir_u and game.is_collision(point_r)) or
-            (dir_d and game.is_collision(point_l)) or
-            (dir_l and game.is_collision(point_u)) or
+            (dir_u and game.is_collision(point_r)) or 
+            (dir_d and game.is_collision(point_l)) or 
+            (dir_l and game.is_collision(point_u)) or 
             (dir_r and game.is_collision(point_d)),
-            
+
             # Danger left
-            (dir_d and game.is_collision(point_r)) or
-            (dir_u and game.is_collision(point_l)) or
-            (dir_r and game.is_collision(point_u)) or
+            (dir_d and game.is_collision(point_r)) or 
+            (dir_u and game.is_collision(point_l)) or 
+            (dir_r and game.is_collision(point_u)) or 
             (dir_l and game.is_collision(point_d)),
             
-            # Move direction (only one of them is true)
+            # Move direction
             dir_l,
             dir_r,
             dir_u,
             dir_d,
             
-            # Food location
+            # Food location 
             game.food.x < game.head.x,  # food left
             game.food.x > game.head.x,  # food right
             game.food.y < game.head.y,  # food up
-            game.food.y > game.head.y,  # food down
+            game.food.y > game.head.y  # food down
         ]
         
         return np.array(state, dtype=int)
